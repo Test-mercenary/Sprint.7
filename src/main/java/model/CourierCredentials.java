77d766625fc6340fec34c268547017c692a23e3b
Andrei Tester <an.myagchilo@gmail.com>
2026-04-22 22:05:18 +0300

    fix: add negative login tests and correct serialization

--- src/main/java/model/CourierCredentials.java
+++ src/main/java/model/CourierCredentials.java
@@ -1,5 +1,8 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourierCredentials {
 
     private String login;
--- src/test/java/courier/LoginCourierTest.java
+++ src/test/java/courier/LoginCourierTest.java
@@ -1,20 +1,19 @@
 package courier;
 
+import client.CourierClient;
+import client.CourierGenerator;
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import model.Courier;
 import model.CourierCredentials;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import client.CourierClient;
-import client.CourierGenerator;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.notNullValue;
-import io.qameta.allure.junit4.DisplayName;
 
 public class LoginCourierTest {
 
@@ -27,6 +26,7 @@ public class LoginCourierTest {
         courierClient = new CourierClient();
         courier = CourierGenerator.getRandom();
         createCourierForLoginTests();
+        courierId = getCourierId();
     }
 
     @After
@@ -55,5 +55,65 @@ public class LoginCourierTest {
                 .then()
                 .statusCode(200)
                 .body("id", greaterThan(0));
+    }
+
+    @Test
+    @DisplayName("Авторизация без логина возвращает ошибку")
+    @Description("Если не передать логин, возвращается 400 и сообщение об ошибке")
+    public void loginWithoutLoginFieldReturnsError() {
+        CourierCredentials noLogin = new CourierCredentials(null, courier.getPassword());
+
+        courierClient.login(noLogin)
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для входа"));
+    }
+
+    @Test
+    @DisplayName("Авторизация с пустым паролем возвращает ошибку")
+    @Description("Если передать пустой пароль, возвращается ошибка")
+    public void loginWithEmptyPasswordReturnsError() {
+        CourierCredentials emptyPassword = new CourierCredentials(courier.getLogin(), "");
+
+        courierClient.login(emptyPassword)
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для входа"));
+    }
+
+    @Test
+    @DisplayName("Авторизация с неверным логином возвращает ошибку")
+    @Description("Если передать неверный логин, возвращается 404 и сообщение об ошибке")
+    public void loginWithWrongLoginReturnsError() {
+        CourierCredentials wrongLogin = new CourierCredentials("wrong_" + courier.getLogin(), courier.getPassword());
+
+        courierClient.login(wrongLogin)
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("Учетная запись не найдена"));
+    }
+
+    @Test
+    @DisplayName("Авторизация с неверным паролем возвращает ошибку")
+    @Description("Если передать неверный пароль, возвращается 404 и сообщение об ошибке")
+    public void loginWithWrongPasswordReturnsError() {
+        CourierCredentials wrongPassword = new CourierCredentials(courier.getLogin(), "wrong_" + courier.getPassword());
+
+        courierClient.login(wrongPassword)
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("Учетная запись не найдена"));
+    }
+
+    @Test
+    @DisplayName("Авторизация несуществующего курьера возвращает ошибку")
+    @Description("Если авторизоваться несуществующим пользователем, возвращается 404 и сообщение об ошибке")
+    public void loginWithNonExistentCourierReturnsError() {
+        CourierCredentials nonExistentCourier = new CourierCredentials("not_exists_login", "not_exists_password");
+
+        courierClient.login(nonExistentCourier)
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("Учетная запись не найдена"));
     }
 }
